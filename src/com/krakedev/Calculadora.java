bdf91e83be8a775a66f029cdf15e7fb959bb15c0
jazzynpro <reader_jaz@hotmail.com>
2026-08-11 20:12:57 -0500

    Primer Commit

---
+++ src/com/krakedev/Calculadora.java
@@ -0,0 +1,18 @@
+package com.krakedev;
+
+public class Calculadora {
+	
+		public int sumar(int a, int b) {
+			int resultado;
+			resultado = a + b;
+			return resultado;
+		}
+		public int restar(int a, int b) {
+			int resultado;
+			resultado = a - b;
+			return resultado;
+		}
+		
+	
+		
+}
---
+++ src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,19 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculadora c1 = new Calculadora();
+		int r1;
+		r1 = c1.sumar(5, 8);		
+		System.out.print("Resultado suma: "+ r1);
+		
+		int r2;
+		r2 = c1.restar(8, 5);
+		System.out.print("Resultado resta: "+ r2);
+		
+	}
+
+}
